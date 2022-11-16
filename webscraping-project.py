418d40b41a3cecf2d37aef0a4da012e7e1ce1c72
Lucas <lucas_layton1@baylor.edu>
2022-11-15 21:50:53 -0600

    done?

--- webscraping-project.py
+++ webscraping-project.py
@@ -2,98 +2,57 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 
-# Webscraping setup
 url = 'https://www.livecoinwatch.com/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-
 req = Request(url, headers=headers)
 webpage = urlopen(req).read()
 soup = BeautifulSoup(webpage, 'html.parser')
 
-# Enter your credentials here
-
 twilio_number = "+12148338562"  
 my_number = "+12149573144"
 account_SID = "AC09187feaaff69211d80225c786aeb1e9"
-token = "6bbde788b0b2633854b801b734d9fb6c"
-
-
-# Twilio setup
+token = "de25322108f13840027e967de8d51649"
 client = Client(account_SID, token)      
+data = soup.findAll("tr", attrs={"class": "table-row filter-row"})
 
-data_rows = soup.findAll("tr", attrs={"class": "table-row filter-row"})
+counter1 = 1
+counter2 = 1
 
+for row in data:
+    cells = row.findAll("td")
+    increase_cells = row.findAll("td", attrs={"class":"filter-item table-item item-sort grow change-col"})
+    decrease_cells = row.findAll("td", attrs={"class":"filter-item table-item item-sort fall change-col"})
+    symbol = cells[counter1].text.split()[0]
+    name = cells[counter1].text.split()[1]
 
-idx_counter = 1
-output_counter = 1
-
-for row in data_rows:
-    data_cells = row.findAll("td")
-
-    # Storing the 24-hr percentages that are growing or falling
-    # Basically, I can't do the calculation without doing this first
-
-    growing_cells = row.findAll("td", attrs={"class":"filter-item table-item item-sort grow change-col"})
-    falling_cells = row.findAll("td", attrs={"class":"filter-item table-item item-sort fall change-col"})
-
-    symbol = data_cells[idx_counter].text.split()[0]
-    name = data_cells[idx_counter].text.split()[1]
-
-    # Adjusting for Cryptocurrency names that are two words or longer
-    if len(data_cells[idx_counter].text.split()) > 2:
+    if len(cells[counter1].text.split()) > 2:
         name_holder = ""
-        for part_of_name in range(1, len(data_cells[idx_counter].text.split())):
-            name_holder += data_cells[idx_counter].text.split()[part_of_name]
+        for part_of_name in range(1, len(cells[counter1].text.split())):
+            name_holder += cells[counter1].text.split()[part_of_name]
             name_holder += " "
         
         name = name_holder.strip(' ')
     
-    current_price = data_cells[idx_counter + 1].text
-    change_over_24_hrs = data_cells[idx_counter + 7].text
+    currentPrice = cells[counter1 + 1].text
+    change24 = cells[counter1 + 7].text
+    calcCurrent = float(currentPrice.replace('$',''))
 
-    current_price_for_calc = float(current_price.replace('$',''))
-
-    
-    if symbol == 'BTC' and current_price_for_calc < 40000:
-        BTC_textmessage = client.messages.create(to=my_number, from_=twilio_number, body="BTC IS BELOW $40000")
+    if symbol == 'BTC' and calcCurrent < 40000:
+        BTC_text = client.messages.create(to=my_number, from_=twilio_number, body="Bitcoin is below $40000")
         
 
-    if symbol == 'ETH' and current_price_for_calc < 3000:
-        ETH_textmessage = client.messages.create(to=my_number, from_=twilio_number, body="ETH IS BELOW $3000")
+    if symbol == 'ETH' and calcCurrent < 3000:
+        ETH_text = client.messages.create(to=my_number, from_=twilio_number, body="Ethereum is below $3000")
      
-    
-    adjusted_24_hr_percent = float(str('.0') + change_over_24_hrs.strip('%').replace('.',''))
-    
-    if data_cells[idx_counter + 7] in falling_cells:
-        adjusted_24_hr_percent *= -1
-
-    # Kinda hard to test this part but basically it was throwing an error because of a weird special
-    # character that only shows up when the % change is 0, can't really test because it's real
-    # time data. The program will still run though.
-
-    elif data_cells[idx_counter + 7] not in falling_cells and data_cells[idx_counter + 7] not in growing_cells:
-        adjusted_24_hr_percent = 0.00
-
-    adjusted_price_based_on_percent = float(current_price.replace('$','')) + float(adjusted_24_hr_percent * float(current_price.replace('$','')))
-
-    # Note: purposely not formatting current_price and adjusted_price_based_on_percent
-    # because it will be easier to grade and showing that the growing and falling code
-    # above works
-
-    # If they were supposed to be rounded, then pretend I used format(current_price, ",.2f")
-    # and format(adjusted_price_based_on_percent, ",.2f")
-
-    # 5 is the limit: aka print the top 5
-    if output_counter <= 5:
-        print(f'Symbol: {symbol}')
-        print(f'Cryptocurrency Name: {name}')
-        print(f'Current price: {current_price}')
-        if data_cells[idx_counter + 7] in falling_cells:
-            print(f'% Change (past 24 hrs): -{change_over_24_hrs}')
+    if counter2 <= 5:
+        print(symbol)
+        print(name)
+        print(calcCurrent)
+        if cells[counter1 + 7] in decrease_cells:
+            print('Percent of change',change24)
         else:
-            print(f'% Change (past 24 hrs): {change_over_24_hrs}')
-        print(f'Price accounting for 24-hr % change: ${adjusted_price_based_on_percent}')
+            print('Percent of change',change24)
         print()
         input()
     
-    output_counter += 1
+    counter2 += 1
